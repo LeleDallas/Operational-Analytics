be22b1ec255d5135ee5c94f36bb6b479ca520d46
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-03-29 11:37:42 +0200

    feat: last part of lesson 9

--- 29-3-2023/lesson9.py
+++ 29-3-2023/lesson9.py
@@ -4,28 +4,41 @@ from statsmodels.graphics.gofplots import qqplot
 import matplotlib.pyplot as plt
 from scipy.stats import shapiro
 from scipy.stats import kstest
+from scipy.stats import sem, t
 
-# data = np.array([-4, -3, 0.8, 1.8, 3.9, 6.2, 6.5])
-# data = np.random.seed()
+data = np.array([-4, -3, 0.8, 1.8, 3.9, 6.2, 6.5])
+data = np.random.seed()
 
-# np.random.seed(20)
-# data = 20 * np.random.rand(100) + 100
-# alpha = 0.05
-# stat, p = shapiro(data)
-# print('Shapiro=%.3f, p=%.3f, alpha=%.3f' % (stat, p, alpha))
-# if p > alpha:
-#     print('Gaussian Sample, no rejection')
-# else:
-#     print('No Gaussian Sample no rejection ')
-# stat, p = kstest(data, 'norm')
-# print('Kolmogorov=%.3f, p=%.3f, alpha=%.3f' % (stat, p, alpha))
-# if p > alpha:
-#     print('Gaussian sample, no rejection')
-# else:
-#     print('No Gaussian sample, rejection')
+np.random.seed(20)
+data = 20 * np.random.rand(100) + 100
+alpha = 0.05
+stat, p = shapiro(data)
+print('Shapiro=%.3f, p=%.3f, alpha=%.3f' % (stat, p, alpha))
+if p > alpha:
+    print('Gaussian Sample, no rejection')
+else:
+    print('No Gaussian Sample no rejection ')
+stat, p = kstest(data, 'norm')
+print('Kolmogorov=%.3f, p=%.3f, alpha=%.3f' % (stat, p, alpha))
+if p > alpha:
+    print('Gaussian sample, no rejection')
+else:
+    print('No Gaussian sample, rejection')
 
-# qqplot(data, line='q')
-# plt.show()
+qqplot(data, line='q')
+plt.show()
+
+
+def ttest(data1, data2, alpha):
+    mean1, mean2 = np.mean(data1), np.mean(data2)
+    se1, se2 = sem(data1),  sem(data2)
+    sed = np.sqrt(se1**2.0 + se2**2.0)
+    t_stat = (mean1-mean2)/sed
+    degf = len(data1) + len(data2) - 2
+    cv = t.ppf(1.0-alpha, degf)
+    p = (1.0 - t.cdf(abs(t_stat), degf))*2.0
+    return t_stat, degf, cv, p
+
 
 df = pd.read_csv(
     'https://raw.githubusercontent.com/mridulrb/Predict-loan-eligibility-using-IBM-Watson-Studio/master/Dataset/Dataset.csv', header=0)
@@ -49,3 +62,10 @@ else:
 
 qqplot(data, line='q')
 plt.show()
+
+#last part
+data1 = df.loc[df["Gender"] == 'Male', 'ApplicantIncome']
+data2 = df.loc[df["Gender"] == 'Female', 'ApplicantIncome']
+t, degf, cv, p = ttest(data1, data2, 0.05)
+
+print(t, degf, cv, p)
